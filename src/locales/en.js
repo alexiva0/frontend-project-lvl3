a72386bc93f460762b25af25c04402c0c87b75fe
Alex Ivanov <alex.ivanov@unity3d.com>
2021-10-08 22:07:47 +0300

    En locale file

---
+++ src/locales/en.js
@@ -0,0 +1,17 @@
+export default {
+  translation: {
+    successMsg: 'RSS successfully added',
+    postButton: 'See more',
+    errors: {
+      default: 'Error',
+      notValidUrl: 'Link should be a valid URL',
+      notUnique: 'RSS already exists',
+      networkError: 'Network error',
+      parseError: 'Resource doesn\'t contain valid RSS',
+    },
+    headers: {
+      feedsHeader: 'Feeds',
+      postsHeader: 'Posts',
+    },
+  },
+};
