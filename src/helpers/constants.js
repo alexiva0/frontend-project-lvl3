02af15a10f5054111351f122282da5b47bcf0caf
Alex Ivanov <alex.ivanov@unity3d.com>
2021-10-13 18:38:28 +0300

    Handle errors proper way

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -9,17 +9,10 @@ const PROXY_GET_URL = 'https://hexlet-allorigins.herokuapp.com/get';
 const PROXY_URL_SEARCH_PARAM = 'url';
 const PROXY_DISABLE_CACHE_SEARCH_PARAM = 'disableCache';
 
-const NETWORK_ERROR_MSG = 'Network Error';
-const PARSING_ERROR_MSG = 'XML Parsing Error';
-const NOT_RSS_ERROR_MSG = 'Cannot read property \'textContent\' of null';
-
 export {
   FORM_STATES,
 
   PROXY_GET_URL,
   PROXY_URL_SEARCH_PARAM,
   PROXY_DISABLE_CACHE_SEARCH_PARAM,
-  NETWORK_ERROR_MSG,
-  PARSING_ERROR_MSG,
-  NOT_RSS_ERROR_MSG,
 };
--- src/service.js
+++ src/service.js
@@ -5,9 +5,6 @@ import {
   PROXY_GET_URL,
   PROXY_URL_SEARCH_PARAM,
   PROXY_DISABLE_CACHE_SEARCH_PARAM,
-  NETWORK_ERROR_MSG,
-  PARSING_ERROR_MSG,
-  NOT_RSS_ERROR_MSG,
 } from './helpers/constants';
 
 const generateFeedId = createIdGenerator();
@@ -21,48 +18,44 @@ const getUrlWithProxy = (feedUrl) => {
 };
 
 const parseFeedResponse = (feedResponse, feedUrl) => {
-  const feedId = generateFeedId();
-  const parser = new DOMParser();
-  const feedXML = parser.parseFromString(
-    feedResponse.data.contents,
-    'application/xml',
-  );
-  const posts = Array.from(feedXML.querySelectorAll('item')).map((item) => ({
-    feedId,
-    id: generatePostId(),
-    guid: item.querySelector('guid').textContent,
-    title: item.querySelector('title').textContent,
-    link: item.querySelector('link').textContent,
-    description: item.querySelector('description').textContent,
-  }));
+  try {
+    const feedId = generateFeedId();
+    const parser = new DOMParser();
+    const feedXML = parser.parseFromString(
+      feedResponse.data.contents,
+      'application/xml',
+    );
+    const posts = Array.from(feedXML.querySelectorAll('item')).map((item) => ({
+      feedId,
+      id: generatePostId(),
+      guid: item.querySelector('guid').textContent,
+      title: item.querySelector('title').textContent,
+      link: item.querySelector('link').textContent,
+      description: item.querySelector('description').textContent,
+    }));
 
-  const feed = {
-    id: feedId,
-    url: feedUrl,
-    title: feedXML.querySelector('title').textContent,
-    description: feedXML.querySelector('description').textContent,
-    visited: false,
-  };
+    const feed = {
+      id: feedId,
+      url: feedUrl,
+      title: feedXML.querySelector('title').textContent,
+      description: feedXML.querySelector('description').textContent,
+      visited: false,
+    };
 
-  return {
-    feed,
-    posts,
-  };
+    return {
+      feed,
+      posts,
+    };
+  } catch (_) {
+    throw new Error('errors.parseError');
+  }
 };
 
 const getFeedData = (feedUrl) => axios
   .get(getUrlWithProxy(feedUrl))
-  .then((feedResponse) => parseFeedResponse(feedResponse, feedUrl))
-  .catch((err) => {
-    if (err.message.includes(NETWORK_ERROR_MSG)) {
-      throw new Error('errors.networkError');
-    }
-
-    if (err.message.includes(PARSING_ERROR_MSG) || err.message.includes(NOT_RSS_ERROR_MSG)) {
-      throw new Error('errors.parseError');
-    }
-
-    throw err;
-  });
+  .catch(() => {
+    throw new Error('errors.networkError');
+  })
+  .then((feedResponse) => parseFeedResponse(feedResponse, feedUrl));
 
 export default getFeedData;
