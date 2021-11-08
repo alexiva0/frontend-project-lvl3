5238415e56674b8269397c1623650ae099415ec4
Alex Ivanov <alex.ivanov@unity3d.com>
2021-11-08 20:14:08 +0200

    Refactor data fetching

--- src/service.js
+++ src/service.js
@@ -17,43 +17,41 @@ const getUrlWithProxy = (feedUrl) => {
   return url.toString();
 };
 
-const parseFeed = (feedString) => {
-  const parser = new DOMParser();
-  return parser.parseFromString(
-    feedString,
-    'application/xml',
-  );
+const parseFeedXml = (feedXML) => {
+  const posts = Array.from(feedXML.querySelectorAll('item')).map((item) => ({
+    guid: item.querySelector('guid').textContent,
+    title: item.querySelector('title').textContent,
+    link: item.querySelector('link').textContent,
+    description: item.querySelector('description').textContent,
+  }));
+  return {
+    title: feedXML.querySelector('title').textContent,
+    description: feedXML.querySelector('description').textContent,
+    posts,
+  };
 };
 
-const generateFeedData = (feedResponse, feedUrl) => {
-  try {
-    const feedXML = parseFeed(feedResponse?.data?.contents);
-    const feedId = generateFeedId();
+const mapFeedData = (feedData, feedUrl) => {
+  const feedId = generateFeedId();
 
-    const posts = Array.from(feedXML.querySelectorAll('item')).map((item) => ({
-      feedId,
-      id: generatePostId(),
-      guid: item.querySelector('guid').textContent,
-      title: item.querySelector('title').textContent,
-      link: item.querySelector('link').textContent,
-      description: item.querySelector('description').textContent,
-    }));
+  const posts = feedData.posts.map((post) => ({
+    ...post,
+    feedId,
+    id: generatePostId(),
+  }));
 
-    const feed = {
-      id: feedId,
-      url: feedUrl,
-      title: feedXML.querySelector('title').textContent,
-      description: feedXML.querySelector('description').textContent,
-      visited: false,
-    };
+  const feed = {
+    ...feedData,
+    posts,
+    id: feedId,
+    url: feedUrl,
+    visited: false,
+  };
 
-    return {
-      feed,
-      posts,
-    };
-  } catch (_) {
-    throw new Error('errors.parseError');
-  }
+  return {
+    feed,
+    posts,
+  };
 };
 
 const getFeedData = (feedUrl) => axios
@@ -61,6 +59,17 @@ const getFeedData = (feedUrl) => axios
   .catch(() => {
     throw new Error('errors.networkError');
   })
-  .then((feedResponse) => generateFeedData(feedResponse, feedUrl));
+  .then((feedResponse) => {
+    const parser = new DOMParser();
+    const feedXML = parser.parseFromString(
+      feedResponse?.data?.contents,
+      'application/xml',
+    );
+    const feedData = parseFeedXml(feedXML);
+    return mapFeedData(feedData, feedUrl);
+  })
+  .catch(() => {
+    throw new Error('errors.parseError');
+  });
 
 export default getFeedData;
